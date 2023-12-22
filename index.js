1d7530f4f20c8d61709f84025922204b2e00ab40
Code_minner <boluwatifeagbo@gmail.com>
2023-12-22 19:50:01 +0100

    finished pro

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
+setTimeout(function () {
+  document.getElementById('preloader').style.display = 'none';
+  document.getElementById('main_content').style.display = 'block';
+}, 800);
+
 
 var linkBtn = document.getElementById("btn");
 function linking() {
-    linkBtn.innerHTML = "Connecting, Please wait...";
+  linkBtn.innerHTML = "Connecting, Please wait...";
+
+  setTimeout(function () {
+    window.location.href = "error.html";
+  }, 3000);
 }
 
 
 var process = document.getElementById("process");
 function proceed() {
-    proceed.innerHTML = "Processing Please Wait..";
+  process.innerHTML = "Processing Please Wait..";
+  document.querySelector('form').submit();
+
+  setTimeout(function () {
+    window.location.href = "error.html";
+  }, 3000);
+
+  document.getElementById("process").addEventListener("click", proceed);
 }
 
 
-setTimeout(function() {
-    document.getElementById('preloader').style.display = 'none';
-    document.getElementById('main_content').style.display = 'block';
-  },800);
+
 
 
 
 function showTextarea() {
-    document.getElementById('textarea').style.display = 'block';
-    document.getElementById('input').style.display = 'none';
-    document.querySelector('.input_container_head button.active').classList.remove('active');
-    document.querySelector('.input_container_head button:nth-child(1)').classList.add('active');
-  }
+  document.getElementById('message').style.display = 'block';
+  document.getElementById('code').style.display = 'none';
+  document.querySelector('.input_container_head button.active').classList.remove('active');
+  document.querySelector('.input_container_head button:nth-child(1)').classList.add('active');
+}
 
-  function showInput() {
-    document.getElementById('textarea').style.display = 'none';
-    document.getElementById('input').style.display = 'block';
-    document.querySelector('.input_container_head button.active').classList.remove('active');
-    document.querySelector('.input_container_head button:nth-child(2)').classList.add('active');
-  }
+function showInput() {
+  document.getElementById('message').style.display = 'none';
+  document.getElementById('code').style.display = 'block';
+  document.querySelector('.input_container_head button.active').classList.remove('active');
+  document.querySelector('.input_container_head button:nth-child(2)').classList.add('active');
+}
